728bbd0ad4caa28a2a65e44c33575f4b728fad29
Pedro de Medeiros <pedro.medeiros@gmail.com>
2026-08-17 16:44:29 -0300

    Run mypy.sh inside virtualenv

--- mypy.sh
+++ mypy.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+# execute in script's directory
+OLD_CD=$PWD
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null || exit 1
+
+# activate python virtual env if not already active
+if [[ ! -n "$VIRTUAL_ENV" ]]; then
+        if [ -f "./.venv/bin/activate" ]; then
+                echo "Virtualenv detected but not active, activating it..."
+                source ./.venv/bin/activate
+        fi
+fi
+
 python -m mypy --config-file=pyproject.toml .
